cd73ce55eeb22c88cec72d36f888ef6737efe6a1
PythonNiku397 <galinschi.nicolae@gmail.com>
2024-04-10 16:44:16 +0300

    subclasses

---
+++ py_final.py
@@ -0,0 +1,36 @@
+# Sarcina nr. 1 "Sarcina Zoopark"
+# class Animal:
+#     def __init__(self, specie, an):
+#         self.specie = specie
+#         self.an = an
+#     def afisare_detalii(self):
+#         print(f"Specie: {self.specie}; Ani: {self.an}")
+# class Mammal(Animal):
+#     def __init__(self, specie, an, nume, rasa, sunet):
+#         super().__init__(specie, an)
+#         self.nume = nume
+#         self.rasa = rasa
+#         self.sunet = sunet
+#     def print_results(self):
+#         print(f"Nume: {self.nume}; Rasa: {self.rasa}; Sunet: {self.sunet}")
+# Tigru = Mammal("Tigru", 14, "Belii Tigrr", "White Tiger", "ROAR-ROARRR")
+# Tigru.afisare_detalii()
+# Tigru.print_results()
+# Sarcina nr.2 "Sarcina Calculator Pentru Impartire"
+# class Calculator:
+#     def __init__(self, test1, test2):
+#         self.test1 = test1
+#         self.test2 = test2
+#         try:
+#             print(float(test1/test2))
+#         except TypeError:
+#             print("Wai, tu stii care e diferenta din litere si numere, sau tu poti imparti si litere?!?")
+#         except ZeroDivisionError:
+#             print("Wai, daca ai 3 mere si imparti la 0 copii cate mere iti raman?!?")
+#         else:
+#             print("Molodetz :D")
+# calculator = Calculator(9, 3)
+
+
+
+
